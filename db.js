11551856eb824d7209470759a1703e80d00ce203
Samuel Warrie <samuelwarrie735@gmail.com>
2023-03-18 22:57:13 +0100

    solved a promise issue

--- db.js
+++ db.js
@@ -5,17 +5,19 @@ const MONGODB_CONNECTION_URI = process.env.MONGODB_CONNECTION_URI;
 
 mongoose.set("strictQuery", false);
 
-//connect to MongoDB
 function connectToMongoDB() {
-  mongoose.connect(MONGODB_CONNECTION_URI);
-
-  mongoose.connection.on("connected", async () => {
-    console.log("Connected to MongoDB successfully");
-  });
+  mongoose
+    .connect(MONGODB_CONNECTION_URI)
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    })
+    .catch((err) => {
+      console.log("Error connecting to MongoDB", err);
+    });
 
   mongoose.connection.on("error", (err) => {
     console.log("Error connecting to MongoDB", err);
   });
 }
-
 module.exports = { connectToMongoDB };
+
