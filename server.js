d39ee8d015c9ffd8433fe8be28ae149b0b6adbc5
Samuel Warrie <samuelwarrie735@gmail.com>
2023-03-19 07:09:58 +0100

    commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(sessionMiddleware);
 
 io.use((socket, next) => sessionMiddleware(socket.request, {}, next));
 
-const PORT = 3500 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
 //Setting the static path
 app.use(express.static("public"));
